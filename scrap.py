e3e639ef51be2407c5f9032d2ebeecbbf5e4cf20
WenqiangWang98 <36535022+WenqiangWang98@users.noreply.github.com>
2022-06-22 21:53:01 +0200

    Add files via upload

--- scrap.py
+++ scrap.py
@@ -4,19 +4,39 @@ pd.set_option('display.max_colwidth', 500)
 import time
 import requests
 import random
+import re
+import unicodedata
+import csv
+name = []
+fam = []
+location = []
+page = []
+page.append( requests.get(
+    "http://www.rjb.csic.es/jardinbotanico/jardin/index.php?Cab=8&SubCab=548&len=en&Pag=549"))
+pag1 = "http://www.rjb.csic.es/jardinbotanico/jardin/index.php?Cab=8&SubCab=548&len=en&Pag=549&Caracter="
 
-url = "http://www.rjb.csic.es/jardinbotanico/jardin/index.php?Cab=8&SubCab=548&len=en&Pag=549"
-page = requests.get(url)
-html = page.text
-pattern = "<em.*?>.*?</em.*?>"
-title_index = html.find("<em>")
-start_index = title_index + len("<em>")
-end_index = html.find("</em>")
-em1 = html[start_index:end_index]
+for i in range(ord('B'), ord('Z')+1):
+    page.append(requests.get(pag1+chr(i)))
+for i in range(len(page)):
 
-title_index = html.find("<em>")
-start_index = title_index + len("<em>")
-end_index = html.find("</em>")
+    soup = bs(page[i].content)
+    quotes = [unicodedata.normalize("NFKD", i.text)
+              for i in soup.find_all(id="descripcion")]
 
-em2=html[start_index:end_index]
-print(em2)
+    for i in range(len(quotes)):
+        nam = quotes[i][1:quotes[i].find("(")].lower()
+        name.append(nam.strip())
+        fam.append(quotes[i][quotes[i].find(": ")+2:quotes[i].rfind(")")-1].lower())
+        loc = (quotes[i][quotes[i].find("n:")+3:quotes[i].rfind("\n")])
+        loc = loc.replace("video", '')
+        loc = loc.replace("foto", '')
+        loc = loc.replace("Foto", '')
+        loc=loc.lower()
+        location.append(loc.strip())
+
+with open('datos.csv', 'w', newline='') as csvfile:
+    spamwriter = csv.writer(csvfile, delimiter=' ',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    for i in range(len(name)):
+        spamwriter.writerow([name[i]] + [fam[i]] + [location[i]])
+
